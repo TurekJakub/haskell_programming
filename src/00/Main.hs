5ecbf5fbc3bd485390d5032dee2ef824251dbd49
TurekJakub <jakubturek32@gmail.com>
2026-03-31 00:09:53 +0200

    refactor

--- src/00/Main.hs
+++ src/00/Main.hs
@@ -1,10 +1,9 @@
 module Main
-  ( main,
-  )
-where
+  ( main
+  ) where
 
 import Control.Arrow
-import Data.Bifunctor (Bifunctor (bimap))
+import Data.Bifunctor (Bifunctor(bimap))
 import Data.Char (isAlpha)
 import Text.Printf (printf)
 
@@ -15,11 +14,12 @@ both :: (Bifunctor f) => (a -> b) -> f a a -> f b b
 both f = bimap f f
 
 calculateVowelPercentage :: String -> Double
-calculateVowelPercentage = (* 100.0) . uncurry (/) . both fromIntegral <$> (countWithFilter (`elem` "aeiouy") &&& countWithFilter isAlpha)
+calculateVowelPercentage =
+  (* 100.0) . uncurry (/) . both fromIntegral
+    <$> (countWithFilter (`elem` "aeiouy") &&& countWithFilter isAlpha)
   where
-    countWithFilter filterFunc = matchingCharsCount filterFunc . filter ((> 3) . length) . words
+    countWithFilter filterFunc =
+      matchingCharsCount filterFunc . filter ((> 3) . length) . words
 
 main :: IO ()
-main =
-  getContents >>= \i ->
-    printf "%.2f%%\n" $ calculateVowelPercentage i
+main = getContents >>= \i -> printf "%.2f%%\n" $ calculateVowelPercentage i
