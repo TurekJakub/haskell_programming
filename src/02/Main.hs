959268b1f5abb13f8f1597b3880c2019fd668722
TurekJakub <jakubturek32@gmail.com>
2026-05-03 21:36:11 +0200

    add basic correctness checking

--- src/02/Main.hs
+++ src/02/Main.hs
@@ -5,7 +5,7 @@ build-depends: base, parser-combinators,containers, megaparsec, pretty, mtl
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE LambdaCase #-}
 
-import Control.Monad (guard, void, when, replicateM_)
+import Control.Monad (guard, void, when, replicateM_, unless, forM_)
 import Control.Monad.Combinators.Expr
 import Data.Bool (bool)
 import Data.Char (chr)
@@ -28,9 +28,13 @@ import Control.Monad.Writer
 import Control.Monad.Reader
 import qualified Data.Map as Map
 import Data.Maybe (fromMaybe)
-import Control.Monad.State (StateT, modify, evalStateT, MonadState (get, put))
+import Control.Monad.State (StateT (runStateT), modify, evalStateT, MonadState (get, put), gets)
 import Data.Containers.ListUtils (nubOrd)
 import Debug.Trace (traceShow, trace)
+import qualified Data.Set as Set
+import Data.Foldable (foldrM)
+import Data.Functor.Classes (Show1(liftShowList))
+import Text.Megaparsec.Char.Lexer (charLiteral)
 
 {- | A data type for tokens. `TBlanks` stores the size of the blank space,
  - because we need it to measure the indentation width. -}
@@ -488,6 +492,139 @@ emitProgram asts =
       code = execWriter $ evalStateT (runReaderT (mapM_ emit asts) env) (Map.size gblMap)
   in allocations ++ code
 
+data ValidationError
+  = UndefinedVariable String
+  | UndefinedFunction String
+  | FunctionRedefinition String
+  | NestedFuncDefinition String
+  | ArityMismatch String Int Int
+  | NotAFunction String
+  | NotAVariable String
+  deriving (Eq, Ord, Show)
+
+type CheckEnv = Map String Int
+
+type CheckState = Set.Set String
+
+type ErrorContext = [String]
+data CheckError = AnalysisError
+  { errorType    :: ValidationError
+  , errorContext :: ErrorContext
+  } deriving (Show)
+
+type CorrectnessCheck a = ReaderT CheckEnv (ReaderT ErrorContext (StateT CheckState (Writer [CheckError]))) a
+
+checkFuncExists :: String -> CheckEnv -> Bool
+checkFuncExists name env = Map.member name env
+
+addFunc :: String -> Int -> CheckEnv -> CheckEnv
+addFunc name arity env = Map.insert name arity env
+
+lookupFuncArity :: String -> CorrectnessCheck (Maybe Int)
+lookupFuncArity name = do asks (Map.lookup name)
+
+addVar :: String -> CorrectnessCheck ()
+addVar name = do 
+  lift . lift $ modify (Set.insert name)
+
+checkVarExists :: String -> CorrectnessCheck Bool
+checkVarExists name = do
+  lift . lift $ gets (Set.member name) 
+
+logError :: ValidationError -> CorrectnessCheck ()
+logError err = do
+  ctx <- lift ask
+  tell [AnalysisError err ctx]
+
+withContext :: String -> CorrectnessCheck a -> CorrectnessCheck a
+withContext newContext  = mapReaderT (local (newContext :)) 
+
+checkCodeBlock :: [Ast] -> CorrectnessCheck()
+checkCodeBlock block = 
+  forM_ block $ \statement -> work statement where
+    work :: Ast -> CorrectnessCheck()
+    work (FunctionDefinition name args body) = do 
+      logError $ NestedFuncDefinition name
+      check $ FunctionDefinition name args body
+    work s =  check s
+
+collectFunctions :: [Ast] -> (CheckEnv, [CheckError])
+collectFunctions = foldr collect (Map.fromList [("print", 1), ("read", 0)], [])
+  where
+    collect :: Ast -> (CheckEnv, [CheckError]) -> (CheckEnv, [CheckError])
+    collect (FunctionDefinition name args _) (env, errs) =
+      if checkFuncExists name env
+        then (env, AnalysisError (FunctionRedefinition name) [] : errs)
+        else (addFunc name (length args) env, errs)
+    collect _ acc = acc
+
+check :: Ast -> CorrectnessCheck ()
+
+check (Variable name)  = do
+  exists <- checkVarExists name 
+  unless exists  (logError $ UndefinedVariable name)
+
+check (Assignment name rhs) = withContext ("in assignment to '" ++ name ++ "'") $ do
+  check rhs
+  addVar name
+
+check (FunctionCall name args) = withContext ("in call to '" ++ name ++ "'") $ do
+  expectedArityMaybe <- lookupFuncArity name
+  varExists <- checkVarExists name
+  case expectedArityMaybe of
+    Nothing -> if varExists then  logError (NotAFunction name) else logError (UndefinedFunction name)
+    Just expectedArity -> do
+      let actualArity = length args
+      when (expectedArity /= actualArity) $
+        logError (ArityMismatch name expectedArity actualArity)
+  mapM_ check args
+
+check (FunctionDefinition name args body) =
+  withContext ("in definition of function '" ++ name ++ "'") $ do
+    originalScope <- get
+    modify (Set.union (Set.fromList args))
+    checkCodeBlock body
+    put originalScope
+
+check (Loop cond body) = 
+  withContext ("in loop") $ do
+      check cond
+      checkCodeBlock body
+
+check (Condition cond thenBlock elseBlock) = 
+  withContext ("in condition") $ do 
+      check cond
+      checkCodeBlock thenBlock
+      case elseBlock of
+        Just block -> checkCodeBlock block
+        Nothing -> return ()
+
+check (BinaryExpression op lhs rhs) = 
+  withContext "in expression "$ do
+  check lhs
+  check rhs
+
+check (IntLiteral _) = do 
+  return ()
+
+check (CharLiteral _) = do
+  return ()
+
+check Pass = do
+  return ()
+
+prettyPrintErrors :: [CheckError] -> IO ()
+prettyPrintErrors errs = forM_ errs $ \err -> do
+  putStrLn $ "Error: " ++ show (errorType err)
+  forM_ (reverse $ errorContext err) $ \ctx ->
+    putStrLn $ "  .. " ++ ctx
+
+doCorrectnessCheck :: [Ast] -> [CheckError]
+doCorrectnessCheck ast = 
+  let (env, redefinitionErrors) = collectFunctions ast
+      remainingErrors = execWriter (runStateT (runReaderT (runReaderT (mapM_ check ast) env) []) Set.empty)
+  in redefinitionErrors ++ remainingErrors
+
 main = do
   args <- getArgs
   when (null args) $ die "no input file supplied"
@@ -496,8 +633,8 @@ main = do
   case tokenize input contents of
     Right tokens ->
       case parseCode input tokens of
-        Right res ->  let hrotfCode = emitProgram res in
-                      mapM_ putStrLn hrotfCode
+        Right res ->  let errs = doCorrectnessCheck res in
+                      prettyPrintErrors errs
         Left err -> putStrLn $ errorBundlePretty err
     Left err -> print err
 
